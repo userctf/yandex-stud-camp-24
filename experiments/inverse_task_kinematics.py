aec512698a2380eafa161902c3f4ff8508a51f1b
Alexander Kuzmichev <alexk4zmich@gmail.com>
2024-10-14 20:34:07 +0500

    add: limits for inverse task

--- experiments/inverse_task_kinematics.py
+++ experiments/inverse_task_kinematics.py
@@ -7,14 +7,26 @@ def inverse_task(x: int, y: int) -> tuple[int, int]:
     # в миллиметрах
     l1 = 95
     l2 = 160
+    R = l1 + l2
+    r = l2 - l1
+
     y -= 50
+    y = max(y, -50)
+    x = max(x, 65)
+
+    gamma = math.atan2(y, x)
+    x = min(x, int(R * math.cos(gamma)))
+    if y < 0:
+        y = max(y, int(R * math.sin(gamma)))
+    else:
+        y = min(y, int(R * math.sin(gamma)))
+
     Q1 = math.atan(y / x) + math.acos((l1**2 + x**2 + y**2 - l2**2) / (2 * l1 * (x**2 + y**2)**0.5))
     Q2 = math.pi - math.acos((l1**2 + l2**2 - x**2 - y**2) / (2 * l1 * l2))
 
-    alpha = int(81 + Q1 * 180 / math.pi)
-    betta = int(180 - Q2 * 180 / math.pi)
+    alpha = min(int(81 + Q1 * 180 / math.pi), 170)
+    betta = min(int(180 - Q2 * 180 / math.pi), 170)
 
-    print(alpha, betta)
     return alpha, betta
 
 
@@ -52,8 +64,13 @@ def send_command(command: bytearray) -> bool:
         return False
 
 
-alpha, betta = inverse_task(210, 100)
-
+alpha, betta = inverse_task(0, 0)
+default_command = [
+    bytearray([255, 1, 2, 170, 255]),
+    bytearray([255, 1, 1, 170, 255]),
+    bytearray([255, 1, 1, alpha, 255]),
+    bytearray([255, 1, 2, betta, 255]),
+]
 
 commands_prepare_grab = [
     bytearray([255, 1, 2, 123, 255]),
@@ -62,10 +79,10 @@ commands_prepare_grab = [
     bytearray([255, 1, 4, 50, 255]),
 ]
 
-commands_forward_hand = [
-    bytearray([255, 1, 2, betta, 255]),
-    bytearray([255, 1, 1, alpha, 255]),
-]
+# commands_forward_hand = [
+#     bytearray([255, 1, 2, betta, 255]),
+#     bytearray([255, 1, 1, alpha, 255]),
+# ]
 
 commands_make_grab = [
     bytearray([255, 1, 4, 75, 255]),
@@ -75,7 +92,24 @@ commands_make_grab = [
 ]
 
 # run_commands(commands_prepare_grab)
+run_commands(default_command)
+
+alpha, betta = inverse_task(100, 250)
+commands_forward_hand = [
+    bytearray([255, 1, 1, alpha, 255]),
+    bytearray([255, 1, 2, betta, 255]),
+]
 run_commands(commands_forward_hand)
+time.sleep(5)
+alpha, betta = inverse_task(250, 110)
+commands_forward_hand = [
+    bytearray([255, 1, 2, betta, 255]),
+    bytearray([255, 1, 1, alpha, 255]),
+]
+run_commands(commands_forward_hand)
+run_commands(default_command)
+
+
 # run_commands(commands_make_grab)
 
 # Закрываем соединение
