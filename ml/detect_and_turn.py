eb1128d27df58aead94de710d72e863a28cbb934
Yaroslav Starostin <86950684+yistarostin@users.noreply.github.com>
2024-10-15 19:04:36 +0500

    working detect+measure dist (#20)

--- ml/detect_and_turn.py
+++ ml/detect_and_turn.py
@@ -5,6 +5,7 @@ import requests
 import numpy as np
 from time import sleep
 from typing import Tuple, List
+from robot_projection import example
 
 url = "http://192.168.2.106:8080/?action=stream"
 
@@ -15,14 +16,15 @@ class Prediction:
         self.class_name: str = prediction.class_name
         self.confidence: float = prediction.confidence
         self.center: Tuple[float, float] = (prediction.x, prediction.y)
-        self.points = prediction.points
+        self.points = [(point.x, point.y) for point in prediction.points]
 
     def get_delta(self, point: Tuple[float, float]) -> Tuple[float, float]:
-        return (self.center[0] - point[0], self.center[1] -  point[1])
+        mid_down = (self.center[2][0] + self.center[3][0]) / 2, (self.center[2][1] + self.center[3][1]) / 2
+        return (mid_down[0] - point[0], mid_down[1] -  point[1])
 
 def predict(model, image) -> Prediction:
-    results = model.infer(image, confidence=0.8)[0]
-    print(results)
+    results = model.infer(image, confidence=0.7)[0]
+    # print(results)
     return [Prediction(pred) for pred in results.predictions]
 
 
@@ -39,14 +41,25 @@ def read_stream(model):
             print("Started to predict image")
             img = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
             results: List[Prediction] = predict(model, img)
-            for item in results:
-                print(item.__dict__)
+            if len(results) == 0:
+                continue
             best_prediction = results[0]
+            # print(best_prediction.__dict__)
+            print(best_prediction.points)
+
+            mid_down = (best_prediction.points[1][0] + best_prediction.points[2][0]) / 2, ( best_prediction.points[1][1] + best_prediction.points[2][1]) / 2
+            print(mid_down)
+            x, y = mid_down
+
+            dist = example(x, y)
+            print(dist)
+
+
+            """
             x_delta, y_delta = best_prediction.get_delta(IMAGE_CENTER)
             print(x_delta, y_delta)
             if abs(x_delta) > 5:
                 pass
-                """
                 TODO:
                 if x_delta < 0
 
@@ -54,16 +67,11 @@ def read_stream(model):
                 else:
                     turn_right(1 degree)
                 
-
-                """
-
+            """
 
             stream.close()
-            sleep(0.1)
+            sleep(1)
             return
-
-
 model = get_model(model_id="top-camera-detection-r4fqs/2", api_key="uGu8WU7fJgR8qflCGaqP")
-for _ in range(5):
+while True:
     read_stream(model)
-    
