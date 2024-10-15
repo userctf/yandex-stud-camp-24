ca97c95717981cc913515960b6839517cf16f7de
Alexander Kuzmichev <alexk4zmich@gmail.com>
2024-10-15 14:44:22 +0500

    update robot projection to camera on robot

--- experiments/projection/robot_projection.py
+++ experiments/projection/robot_projection.py
@@ -5,18 +5,18 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 
 
-img = cv.imread('image_robot.jpg')
+img = cv.imread('proj_photo_1.jpg')
 assert img is not None, "file could not be read, check with os.path.exists()"
 rows, cols, ch = img.shape
 
 # Define source and destination points
-srcPoints = np.array([[34, 959], [300, 522], [661, 522], [852, 959]], dtype=np.float32)
-dstPoints = np.array([[0, 600*3], [0, 0], [500*3, 0], [500*3, 600*3]], dtype=np.float32)
+srcPoints = np.array([[320, 478], [638, 449], [320, 147], [502, 149]], dtype=np.float32)
+dstPoints = np.array([[200*3, 1000*3], [305*3, (1000-10)*3], [3*200, (1000-290)*3], [305*3, (1000-290)*3]], dtype=np.float32)
 
 # Compute homography matrix
 M, mask = cv.findHomography(srcPoints, dstPoints, cv.RANSAC, 5.0)
 start = time.time()
-dst = cv.warpPerspective(img,M,(500*3,600*3))
+dst = cv.warpPerspective(img,M,(400*3, 1000*3))
 end = time.time()
 
 print(end - start)
