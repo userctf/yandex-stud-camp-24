8b5ffa6b5e06f2fd68bdebd4b546506dd98c2178
Aleksey Skvortsov <46933547+Rattysed@users.noreply.github.com>
2024-10-16 14:58:18 +0500

    update arm controller (#24)

--- modules/module.py
+++ modules/module.py
@@ -6,9 +6,11 @@ class BaseModule:
         self.socket = s
         pass
 
-    def _send(self, message: bytearray, sleep_time: float):
+    def _send(self, message: bytearray, sleep_time: float) -> bool:
         try:
             self.socket.sendall(message)
         except socket.error as e:
             print(f"Ошибка сокета: {e}")
+            return False
         time.sleep(sleep_time)
+        return True
