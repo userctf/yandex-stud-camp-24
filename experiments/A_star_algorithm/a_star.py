3acb96e1a6727632f72d1bedd307565bf49ec2ea
Yaroslav Starostin <yistarostin@gmail.com>
2024-10-17 15:08:03 +0500

    speedup A*

--- experiments/A_star_algorithm/a_star.py
+++ experiments/A_star_algorithm/a_star.py
@@ -1,9 +1,36 @@
 import cv2
+import heapq
+from time import time
 import math
 import matplotlib.pyplot as plt
+from typing import List
 
 show_animation = True
 
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+    
+    def empty(self):
+        return len(self.elements) == 0
+    
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+    
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
+class Node:
+    def __init__(self, x, y, cost, path):
+        self.x = x
+        self.y = y
+        self.cost = cost
+        self.path = path
+    def __lt__(self, other) -> bool:
+        return self.cost < other.cost
+
+    def __str__(self):
+        return str(self.x) + '+' + str(self.y) + ',' + str(self.cost) + ',' + str(self.path)
 
 class AStarPath:
     def __init__(self, robot_radius, grid_size, x_obstacle, y_obstacle):
@@ -11,21 +38,10 @@ class AStarPath:
         self.robot_radius = robot_radius
         self.create_obstacle_map(x_obstacle, y_obstacle)
         self.path = self.get_path()
-
-    class Node:
-        def __init__(self, x, y, cost, path):
-            self.x = x
-            self.y = y
-            self.cost = cost
-            self.path = path
-
-        def __str__(self):
-            return str(self.x) + '+' + str(self.y) + ',' + str(self.cost) + ',' + str(self.path)
-
     @staticmethod
     def calc_heuristic(num1, num2):
-        weight = 1.0
-        return weight * math.sqrt((num1.x - num2.x) ** 2 + (num1.y - num2.y) ** 2)
+        weight = 10.0
+        return weight * (abs(num1.x - num2.x) + abs(num1.y - num2.y))    
 
     def calc_grid_position(self, idx, p):
         return idx * self.grid_size + p
@@ -88,60 +104,65 @@ class AStarPath:
             self.calc_grid_position(end_node.y, self.y_min)]
         path = end_node.path
         while path != -1:
-            n = record_closed[path]
-            x_out_path.append(self.calc_grid_position(n.x, self.x_min))
-            y_out_path.append(self.calc_grid_position(n.y, self.y_min))
-            path = n.path
+            prev_node = record_closed[self.calc_grid_idx(path)]
+            x_out_path.append(self.calc_grid_position(prev_node.x, self.x_min))
+            y_out_path.append(self.calc_grid_position(prev_node.y, self.y_min))
+            path = prev_node.path
 
         return x_out_path, y_out_path
 
     def a_star_search(self, start_x, start_y, end_x, end_y):
-        start_node = self.Node(self.calc_xy(start_x, self.x_min), self.calc_xy(start_y, self.y_min), 0.0, -1)
-        end_node = self.Node(self.calc_xy(end_x, self.x_min), self.calc_xy(end_y, self.y_min), 0.0, -1)
+        end_node = Node(self.calc_xy(end_x, self.x_min), self.calc_xy(end_y, self.y_min), 0.0, -1)
+        start_node = Node(self.calc_xy(start_x, self.x_min), self.calc_xy(start_y, self.y_min), 0.0, -1)
+        start_node.cost = 0
 
         record_open, record_closed = dict(), dict()
         record_open[self.calc_grid_idx(start_node)] = start_node
+        state = PriorityQueue()
+        state.put(start_node, start_node.cost)
 
         while True:
-            if len(record_open) == 0:
+            if len(state.elements) == 0:
                 print('Check Record Validity')
                 break
-
-            total_cost = min(record_open,
-                             key=lambda x: record_open[x].cost + self.calc_heuristic(end_node, record_open[x]))
-            cost_collection = record_open[total_cost]
-
+            
+            closest_node = state.get()
+            if self.calc_grid_idx(closest_node) in record_closed:
+                continue
             if show_animation:  # pragma: no cover
-                plt.plot(self.calc_grid_position(cost_collection.x, self.x_min),
-                         self.calc_grid_position(cost_collection.y, self.y_min), "xy")
+                plt.plot(self.calc_grid_position(closest_node.x, self.x_min),
+                         self.calc_grid_position(closest_node.y, self.y_min), "xy")
                 if len(record_closed.keys()) % 10 == 0:
                     plt.pause(0.001)
 
-            if cost_collection.x == end_node.x and cost_collection.y == end_node.y:
+
+            if closest_node.x == end_node.x and closest_node.y == end_node.y:
                 print("Finished!")
-                end_node.path = cost_collection.path
-                end_node.cost = cost_collection.cost
+                end_node.path = closest_node.path
+                end_node.cost = closest_node.cost
                 break
 
-            del record_open[total_cost]
-            record_closed[total_cost] = cost_collection
+            record_closed[self.calc_grid_idx(closest_node)] = closest_node
 
             for i, _ in enumerate(self.path):
-                node = self.Node(cost_collection.x + self.path[i][0], cost_collection.y + self.path[i][1],
-                                 cost_collection.cost + self.path[i][2], total_cost)
-                idx_node = self.calc_grid_idx(node)
+                next_node = Node(closest_node.x + self.path[i][0],closest_node.y + self.path[i][1],
+                                0,closest_node)
+                next_node.cost = closest_node.cost - self.calc_heuristic(closest_node, end_node) + self.path[i][2] + self.calc_heuristic(next_node, end_node)
 
-                if not self.check_validity(node):
+                idx_node = self.calc_grid_idx(next_node)
+                if not self.check_validity(next_node):
                     continue
 
                 if idx_node in record_closed:
                     continue
-
+                
                 if idx_node not in record_open:
-                    record_open[idx_node] = node
+                    record_open[idx_node] = next_node
+                    state.put(next_node, next_node.cost)
                 else:
-                    if record_open[idx_node].cost > node.cost:
-                        record_open[idx_node] = node
+                    if record_open[idx_node].cost > next_node.cost:
+                        record_open[idx_node] = next_node
+                        state.put(next_node, next_node.cost)
 
         x_out_path, y_out_path = self.calc_final_path(end_node, record_closed)
 
@@ -151,20 +172,20 @@ class AStarPath:
 def main():
     start_x = 30
     start_y = 30
-    end_x = 170
-    end_y = 175
-    grid_size = 4.0
-    robot_radius = 5.0
+    end_x = 175
+    end_y = 125
+    grid_size = 3.0
+    robot_radius = 4.0
 
     image = cv2.imread('output_frame_0089_fixed.png')
-    image = cv2.resize(image, (200, 200))
+    image = cv2.resize(image, (200, 155))
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     (width, length) = gray.shape
     x_obstacle, y_obstacle = [], []
     print(width, length)
     for i in range(width):
         for j in range(length):
-            if gray[i][j] <= 90:
+            if gray[i][j] <= 80:
                 y_obstacle.append(i)
                 x_obstacle.append(j)
 
@@ -174,9 +195,13 @@ def main():
         plt.plot(end_x, end_y, "xb")
         plt.grid(True)
         plt.axis("equal")
-
+    slow = time()
     a_star = AStarPath(robot_radius, grid_size, x_obstacle, y_obstacle)
+    start = time()
     x_out_path, y_out_path = a_star.a_star_search(start_x, start_y, end_x, end_y)
+    end = time()
+    print(end - start)
+    print(end - slow)
 
     if show_animation:
         plt.plot(x_out_path, y_out_path, "r")
