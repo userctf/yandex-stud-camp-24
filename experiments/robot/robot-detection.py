ec8cf837355bab405757352da88d230a6e7e3d1f
Rattysed <46933547+Rattysed@users.noreply.github.com>
2024-10-18 18:40:13 +0500

    initial

--- experiments/robot/robot-detection.py
+++ experiments/robot/robot-detection.py
@@ -1,16 +1,63 @@
+from typing import Tuple
+
 import cv2
-import time
 import os
 import sys
 
+import numpy as np
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../modules')))
 from modules.top_camera import TopCamera
-from modules.base_camera import ObjectType
+from modules.base_camera import ObjectType, Prediction
 
 camera = TopCamera("rtsp://Admin:rtf123@192.168.2.250:554/1", 'detecting_objects-ygnzn/1',
                    api_key="d6bnjs5HORwCF1APwuBX")
 
 image = cv2.imread("left.jpg")
-preds = camera.predict(image)
+im2 = image.copy()
+preds = camera.get_objects(im2, ObjectType.ROBOT)
+image = cv2.cvtColor(image, cv2.RGV)
+cv2.imshow("z", image)
 print(preds)
+
+COLORS = {
+    "red": [
+        (np.array([0, 50, 50]),
+         np.array([10, 255, 255]),),
+        (np.array([170, 50, 50]),
+         np.array([180, 255, 255]),)
+    ],
+    "green": [
+        (np.array([35, 40, 40]), np.array([85, 255, 255])),
+    ]
+}
+
+
+def find_robot_coordinates(image: np.array, robot: Prediction) -> Tuple[int, int, int]:
+    cropped = crop_to_robot(image, robot)
+    check_robot_color(cropped, "green")
+
+    pass
+
+
+def crop_to_robot(image: np.array, robot: Prediction):
+    top, bottom = robot.get_coords()
+    cropped = image[top[1]:bottom[1], top[0]:bottom[0]]
+    cv2.imshow('Original Image', cropped)
+    cv2.waitKey(0)
+    return cropped
+
+def check_robot_color(cropped: np.array, color: str) -> bool:
+    thresholds = COLORS[color]
+    cropped = cropped.copy()
+    res_mask = np.zeros(cropped.shape[0:2], np.uint8)
+    for threshold in thresholds:
+        mask = cv2.inRange(cropped, threshold[0], threshold[1])
+        res_mask = cv2.bitwise_or(res_mask, mask)
+
+    cv2.imshow('Original Image', res_mask)
+    cv2.waitKey(0)
+    return False
+
+for robot in preds:
+    find_robot_coordinates(image, robot)
--- modules/base_camera.py
+++ modules/base_camera.py
@@ -8,9 +8,9 @@ import requests
 
 class ObjectType(Enum):
     CUBE = 0
-    BALL = 1    # it is detecting only by on board cam
+    BALL = 1  # it is detecting only by on board cam
     BASKET = 2  # it is detecting only by on board cam
-    BUTTONS = 3 # it is detecting only by on board cam
+    BUTTONS = 3  # it is detecting only by on board cam
     ROBOT = 4
     GREEN_BASE = 5
     BTN_G_O = 6
@@ -25,31 +25,31 @@ class Prediction:
         self.confidence: float = prediction.confidence
         self.center: Tuple[float, float] = (prediction.x, prediction.y)
         self.size: Tuple[float, float] = (prediction.width, prediction.height)
-        
+
     def get_coords(self) -> Tuple:
         left_top = (
-            int(self.center[0] - 0.5 * self.size[0]), 
+            int(self.center[0] - 0.5 * self.size[0]),
             int(self.center[1] - 0.5 * self.size[1])
         )
         right_bottom = (
-            int(self.center[0] + 0.5 * self.size[0]), 
+            int(self.center[0] + 0.5 * self.size[0]),
             int(self.center[1] + 0.5 * self.size[1])
         )
-        
+
         return left_top, right_bottom
 
     def get_color(self) -> Tuple:
         if self.object_type == ObjectType.BALL:
-            return (0, 115, 255) # orange
+            return (0, 115, 255)  # orange
         elif self.object_type == ObjectType.CUBE:
-            return (0, 10, 255) # red
+            return (0, 10, 255)  # red
         elif self.object_type == ObjectType.BASKET:
-            return (28, 28, 28) # black
+            return (28, 28, 28)  # black
         elif self.object_type == ObjectType.BUTTONS:
-            return (94, 81, 81) # grey
+            return (94, 81, 81)  # grey
         elif self.object_type == ObjectType.ROBOT:
-            return (203, 89, 23) # blue
-        
+            return (203, 89, 23)  # blue
+
     @staticmethod
     def __get_type(name: str) -> ObjectType:
         if name == "red cube":
@@ -78,7 +78,7 @@ class Prediction:
 
 class BaseCamera:
     CONFIDENCE = 0.5
-    
+
     def __init__(self, stream_url: str, neural_model: str, api_key: str):
         print("[INFO] start initiating camera")
         self.stream_url = stream_url
@@ -98,42 +98,41 @@ class BaseCamera:
         for pred in predictions:
             left_up, right_down = pred.get_coords()
             color = pred.get_color()
-            
+
             white_color = (255, 255, 255)  # White color in BGR
-            cv2.putText(frame, pred.name + ":" + str(round(pred.confidence, 3)), (left_up[0], left_up[1] - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, white_color, 2)
+            cv2.putText(frame, pred.name + ":" + str(round(pred.confidence, 3)), (left_up[0], left_up[1] - 20),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, white_color, 2)
             cv2.rectangle(frame, left_up, right_down, color, 2)
-    
+
     # working with neural model
     def get_objects(self, frame: numpy.ndarray, object: ObjectType) -> List[Prediction]:
         found_objects = self.predict(frame)
-        
+
         correct_objects = filter(lambda predict: predict.object_type.value == object.value, found_objects)
         return sorted(correct_objects, key=lambda pred: pred.confidence, reverse=True)
-                
+
     def predict(self, frame: numpy.ndarray) -> List[Prediction]:
         results = self.model.infer(frame, confidence=self.CONFIDENCE)[0]
         return [Prediction(pred) for pred in results.predictions]
-    
+
     def __del__(self):
         self.cap.release()
-    
+
     # test
     def test(self):
         while True:
             img = self.get_photo()
             p = self.predict(img)
             self._write_on_img(img, p)
-            
+
             cv2.imshow('Frame with Box', img)
-            
+
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-        
+
         cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
     BC = BaseCamera("http://192.168.2.106:8080/?action=stream", "robot_camera_detector/1", "uGu8WU7fJgR8qflCGaqP")
     BC.test()
-
-
