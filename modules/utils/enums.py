ceab2ab14ea4abfa8a6ead603836190b79d29b57
Rattysed <46933547+Rattysed@users.noreply.github.com>
2024-10-21 10:28:39 +0500

    add find-robot into GameMap

---
+++ modules/utils/enums.py
@@ -0,0 +1,35 @@
+from enum import Enum
+
+
+class GameObjectPosition(Enum):
+    HORIZONTAL = 0
+    VERTICAL = 1
+    INNER_START_LIKE = 2
+    INNER_COUNTER_START_LIKE = 3
+    OUTER_START_LIKE = 5
+    OUTER_COUNTER_START_LIKE = 6
+    UNKNOWN = -1
+
+
+class GameObjectType(Enum):
+    CUBE = 0
+    BALL = 1
+    ANY_ROBOT = 4
+    GREEN_BASE = 5
+    BTN_GREEN_ORANGE = 6
+    BTN_PINK_BLUE = 7
+    RED_BASE = 8
+    OUR_ROBOT = 9
+    BAD_ROBOT = 10
+    UNKNOWN = -1
+
+class ObjectType(Enum):
+    CUBE = 0
+    BALL = 1  # it is detecting only by on board cam
+    BASKET = 2  # it is detecting only by on board cam
+    BUTTONS = 3  # it is detecting only by on board cam
+    ROBOT = 4
+    GREEN_BASE = 5
+    BTN_G_O = 6
+    BTN_P_B = 7
+    UNKNOWN = -1
