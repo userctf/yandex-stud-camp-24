5b3a7006af1296120350287367b6517a1bc1a6c1
Yaroslav Starostin <yistarostin@gmail.com>
2024-10-21 14:35:38 +0500

    move incldues to utils folder

---
+++ modules/utils/position.py
@@ -0,0 +1,22 @@
+class Position:
+    def __init__(self, x: int, y: int, angle: float = 0):
+        self.x: int = x
+        self.y: int = y
+        self.angle: float = angle
+
+    def __iter__(self):
+        yield self.x
+        yield self.y
+        yield self.angle
+
+    def __add__(self, other):
+        return Position(self.x + other.x, self.y + other.y, self.angle)
+
+    def __iadd__(self, other):
+        self.x += other.x
+        self.y += other.y
+        return self
+
+    def __repr__(self):
+        return f"({self.x}, {self.y}), angle is {self.angle}"
+
